c2fc19769593249fe8a1d6cff581066d71510275
manan-bedi2908 <mananbedilps@gmail.com>
2024-04-21 15:25:41 +0530

    gemini response func added

--- app.py
+++ app.py
@@ -10,4 +10,6 @@ genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 def get_gemini_response(input, image, prompt):
     model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content(input, image[0], prompt)
+    return response.text
 
