6fe586187eab18f46438c617e65f3e1bce9f84dd
manan-bedi2908 <mananbedilps@gmail.com>
2024-04-21 15:53:01 +0530

    model building completed

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 def get_gemini_response(input, image, prompt):
     model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content(input, image[0], prompt)
+    response = model.generate_content([input, image[0], prompt])
     return response.text
 
 def input_image_setup(uploaded_file):
     if uploaded_file is not None:
-        bytes_data = uploaded_file.getValue()
+        bytes_data = uploaded_file.getvalue()
         image_parts = [
             {
                 'mime_type': uploaded_file.type,
@@ -45,4 +45,8 @@ You are an expert in understandinhg invoices. You will receive
 input images as invoices and you will have to answer questions
 based on the input image.
 """
-
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response = get_gemini_response(input_prompt, image_data, input)
+    st.subheader('The Response is: ')
+    st.write(response)
