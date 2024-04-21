78cc17d866103f13a058d2f95cc59b4aebef8835
manan-bedi2908 <mananbedilps@gmail.com>
2024-04-21 15:23:05 +0530

    initialized project

---
+++ app.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import streamlit as st
+import os
+from PIL import Image
+import google.generativeai as genai
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def get_gemini_response(input, image, prompt):
+    model = genai.GenerativeModel()
+
